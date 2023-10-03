5f8cc3b612179168f2e1351019435e46675e5ee6
Tomoya <c0a2209178@edu.teu.ac.jp>
2023-10-03 14:43:50 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,13 +5,24 @@ import random
 WIDTH, HEIGHT = 1600, 900
 
 
+delta = {#辞書作成
+    pg.K_UP:(0,-5),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0)
+}
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     bd_img = pg.Surface((20, 20)) #爆弾サーフェイス作成
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900,400)#工科トンの初期座標
+
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect()
     x,y = random.randint(0,WIDTH),random.randint(0,HEIGHT) #乱数設定
@@ -19,6 +30,9 @@ def main():
     vx,vy = +5,+5
     bd_img.set_colorkey((0, 0, 0))
     
+    
+
+
     clock = pg.time.Clock()
     tmr = 0
     
@@ -29,9 +43,20 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        #工科とん始まり
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]#横方向の合計
+                sum_mv[1] += mv[1]#縦方向の合計
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+
+        screen.blit(kk_img,kk_rct)
+        #爆弾始まり
         bd_rct.move_ip(vx,vy)
         screen.blit(bd_img,bd_rct) #試しにrectを使ってblit
         pg.display.update()
