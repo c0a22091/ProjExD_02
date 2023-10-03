3eb39549ac19648b1d7fe2cf04e560e329bd39cd
Tomoya <c0a2209178@edu.teu.ac.jp>
2023-10-03 14:24:45 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -16,6 +16,7 @@ def main():
     bd_rct = bd_img.get_rect()
     x,y = random.randint(0,WIDTH),random.randint(0,HEIGHT) #乱数設定
     bd_rct.center = (x,y)
+    vx,vy = +5,+5
     bd_img.set_colorkey((0, 0, 0))
     
     clock = pg.time.Clock()
@@ -31,10 +32,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bd_rct.move_ip(vx,vy)
         screen.blit(bd_img,bd_rct) #試しにrectを使ってblit
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
     
     
 
